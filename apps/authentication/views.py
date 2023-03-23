69ab828f80109f1bf01e2a18a66dae76bc1b75d7
santiagocorralesv <santi368444110@gmail.com>
2023-03-23 05:45:53 -0500

    refactor: add comments

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.views import LoginView
 
 class Login(LoginView):
     """
-    
+    Login view that receives username and password.
+    All logic is implemented by the inherited class Django LoginView
     """
     template_name = 'authentication/login/login.html'
     redirect_authenticated_user = True
